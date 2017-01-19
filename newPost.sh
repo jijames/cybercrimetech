d97ebf9865245f4decb8e34a611802b9b0bc916f
Joshua <joshua.i.james@gmail.com>
2017-01-19 21:29:00 +0900

    test post

---
+++ newPost.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A script to make a basic new post given a title
+
+if [ "$1" ]; then
+
+DATE=$(date --iso)
+DATETIME=$(date --iso=seconds)
+TITLE=$(echo "$1" | sed 's/ /-/g')
+FILENAME="$DATE-${TITLE}.html"
+POST="_posts/$FILENAME"
+
+echo "---" > $POST
+echo "layout: single" >> $POST
+echo "title: '${1}'" >> $POST
+echo "date: '$DATETIME'" >> $POST
+echo "author: Joshua I. James" >> $POST
+echo "tags:" >> $POST
+echo "- infosec" >> $POST
+echo "- dfir" >> $POST
+echo "modified_time: '$DATETIME'" >> $POST
+echo "---" >> $POST
+
+xed $POST &
+fi
